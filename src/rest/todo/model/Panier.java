19d90f757be90c287c107e1ec8a434a391d25efc
LydiaSM <l.smahi@hotmail.fr>
2022-02-17 14:44:39 +0100

    Modif Panier

--- src/rest/todo/model/Panier.java
+++ src/rest/todo/model/Panier.java
@@ -1,27 +1,28 @@
 package rest.todo.model;
 
 public class Panier {
-	
-	
-	private int id;
-	private double prixTotal;
+	private int idpanier;
+	private double prixtotal;
 	  
-	public Panier(int id, double prixTotal) {
-		this.id=id;
-		this.prixTotal=prixTotal;
-	}
-	  public int getId() {
-		return id;
-	}
-	  
-	public void setId(int id) {
-		this.id = id;
-	}
-	public double getPrixTotal() {
-		return prixTotal;
-	}
-	public void setPrixTotal(double prixTotal) {
-		this.prixTotal = prixTotal;
+	public Panier(int idpanier, double prixtotal) {
+		this.idpanier = idpanier;
+		this.prixTotal = prixtotal;
 	}
 
+	public int getIdpanier() {
+		return idpanier;
+	}
+
+	public void setIdpanier(int idpanier) {
+		this.idpanier = idpanier;
+	}
+
+	public double getPrixtotal() {
+		return prixtotal;
+	}
+
+	public void setPrixtotal(double prixtotal) {
+		this.prixtotal = prixtotal;
+	}
+	
 }
