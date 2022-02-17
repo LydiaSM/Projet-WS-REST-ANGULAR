616b5fce27eb9fa6e40d253cea073daff8224ed6
Projet collaboratif Master 1 MIAGE <87546751+aissatoubarryfab@users.noreply.github.com>
2022-02-17 14:41:00 +0100

    Panier

--- src/rest/todo/model/Panier.java
+++ src/rest/todo/model/Panier.java
@@ -1,5 +1,27 @@
 package rest.todo.model;
 
 public class Panier {
+	
+	
+	private int id;
+	private double prixTotal;
+	  
+	public Panier(int id, double prixTotal) {
+		this.id=id;
+		this.prixTotal=prixTotal;
+	}
+	  public int getId() {
+		return id;
+	}
+	  
+	public void setId(int id) {
+		this.id = id;
+	}
+	public double getPrixTotal() {
+		return prixTotal;
+	}
+	public void setPrixTotal(double prixTotal) {
+		this.prixTotal = prixTotal;
+	}
 
 }
